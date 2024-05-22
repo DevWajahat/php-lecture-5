7c18a94ed8054197e7f4bee4eb54af0ece1dfdb7
Wajahat Abbas <155806602+DevWajahat@users.noreply.github.com>
2024-05-22 20:49:28 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// $num = 1;
+
+// if ($num==1){
+//     echo "Good";
+// }
+// else if($num==2){
+//     echo "Start";
+// }
+// else if($num==3){
+//     echo "nice";
+// }
+// else{
+//     echo "unknown selection";
+// }
+
+// $num=30;
+// switch($num){
+//     case 1: 
+//         echo "Marvelous";
+//         break;
+//     case 2: 
+//         echo "Star";
+//         break;
+//     case 30:
+//         echo "Good";
+//         break;
+//     default:
+//     echo "unknown selection";
+//     break;
+// }
+
+$num="Java";
+switch($num){
+    case "Java": 
+        echo "Marvelous";
+        break;
+    case "Phython": 
+        echo "Star";
+        break;
+    case "XML":
+        echo "Good";
+        break;
+    default:
+    echo "unknown selection";
+    break;
+}
+?>
---
+++ readme.md
@@ -0,0 +1,8 @@
+Start
+Create Variable num
+assign value
+applying else if condition
+1 if(num==1){ print "Good"}
+2 elseif(num==2){print "Better"}
+3 elseif (num==3){print "Worst"}
+else{print "unknown selection"}
